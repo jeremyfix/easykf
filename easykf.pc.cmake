ca7f28e919805082a6ef6b1e719458040e95c440
Jeremy Fix <jeremy.fix@gmail.com>
2014-07-24 14:26:57 +0200

    devel branch creation

--- easykf.pc.cmake
+++
@@ -1,6 +0,0 @@
-Name: EasyKF
-Description: C++ Library for Extended, Unscented Kalman Filters
-Requires: gsl
-Version: @EASYKF_VERSION@
-Libs: -L@LIB_INSTALL_DIR@ -leasykf
-Cflags: -I@INCLUDE_INSTALL_DIR@
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "ukf.h"
-
-int main(int argc, char * argv[])
-{
-
-}
